23b44c3d6aa956acadfe587a920276b08808b199
Востров Михаил Андреевич <vostrow.mihail@yandex.ru>
2026-02-06 03:26:39 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -4,15 +4,6 @@ async function loadComponent(componentName, targetElement) {
     // Всегда поднимаемся на один уровень вверх из любой папки со страницей
     const hasSubfolder = window.location.pathname.split('/').filter(Boolean).length > 1;
 const basePath = hasSubfolder ? '../' : './';
-
-if (currentPath === '/' || 
-        currentPath.endsWith('/index.html') || 
-        currentPath.endsWith('/') ||
-        currentPath === '/Academy/' || // Для GitHub Pages
-        currentPath === '/Academy/index.html') {
-      basePath = './'; // Компоненты в той же папке
-      console.log('Main page detected, using ./');
-    }
     
     const response = await fetch(`${basePath}components/${componentName}.html`);
     if (!response.ok) {
